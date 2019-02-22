3c76dfbabe893a79d2c110a3283e604310094d66
ibrahimorhan <44341145+ibrahimorhan@users.noreply.github.com>
2019-02-22 21:52:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,207 +1,123 @@
 #include <iostream>
-
+#include <list>
 #include <fstream>
-
 #include <vector>
-
-#include <stack>
-
+#include <limits>
+#include <queue>
+#include <cmath>
 using namespace std;
+int table[1002][1002]={0};
+bool marked[1002][1002]={false};
+class Edge{
+public:
+    int sourceX;
+    int sourceY;
+    int destX;
+    int destY;
+    int distance;
+    Edge(int sX,int sY,int dX,int dY,int d){
 
-struct Node{
 
-    int data;
-
-    int index=-1;
-
-    int lowLink=-2;
-
-    bool onStack=false;
-
-    vector<int> vector1;
-
+        this->sourceX=sX;
+        this->sourceY=sY;
+        this->destX=dX;
+        this->destY=dY;
+        this->distance=d;
+    }
+    struct Bigger{
+        bool operator()(const Edge& distance1,const Edge& distance2){
+            return distance1.distance>distance2.distance;
+        }
+    };
 };
 
-int index=0;
+int LeastLeader(int sX,int sY,int dX,int dY){
 
-stack<int> s;
+    Edge src(sX,sY,sX,sY,0);
+    priority_queue<Edge,vector<Edge>,Edge::Bigger>* q1 =new priority_queue<Edge,vector<Edge>,Edge::Bigger>();
 
-Node boxes [100100];
+    q1->push(src);
+    while(!q1->empty()){
+        vector<Edge> temp;
 
-int sameScc[100100];
+        vector<Edge>::iterator itr;
 
-vector<vector<int>> members;
-
-int sccCount=0;
-
-void strongConnect(Node & n) {
-
-    n.index = index;
-
-    n.lowLink = index;
-
-    index++;
-
-    s.push(n.data);
-
-    n.onStack = true;
-
-    vector<int>::iterator itr;
-
-    for(itr=n.vector1.begin(); itr!=n.vector1.end();++itr){
-
-        int to=*itr;
-
-        if(boxes[to].index==-1){
-
-            strongConnect(boxes[to]);
-
-            n.lowLink=min(n.lowLink,boxes[to].lowLink);
-
+        if(table[q1->top().destX+1][q1->top().destY]!=0){
+            if(!marked[q1->top().destX+1][q1->top().destY]){
+                Edge right(q1->top().destX,q1->top().destY,q1->top().destX+1,q1->top().destY,max(q1->top().distance,abs(table[q1->top().destX][q1->top().destY]-table[q1->top().destX+1][q1->top().destY])));
+                // cout<<q1->top().distance<<endl;
+                //  cout<<abs(table[q1->top().sourceX][q1->top().sourceY]-table[q1->top().sourceX+1][q1->top().sourceY])<<endl;
+                // cout<<right.distance<<endl;
+                //cout<<max(q1->top().distance,abs(table[q1->top().sourceX][q1->top().sourceY]-table[q1->top().sourceX+1][q1->top().sourceY]))<<endl;
+                temp.push_back(right);
+            }
+        }
+        if(table[q1->top().destX-1][q1->top().destY]==0) {
+            //  cout << "limit" << endl;
+        }else{
+            if(!marked[q1->top().destX-1][q1->top().destY]){
+                Edge left(q1->top().destX,q1->top().destY,q1->top().destX-1,q1->top().destY,max(q1->top().distance,abs(table[q1->top().destX][q1->top().destY]-table[q1->top().destX-1][q1->top().destY])));
+                temp.push_back(left);
+                //cout<<left.distance<<endl;
+            }
+        }
+        if(table[q1->top().destX][q1->top().destY+1]!=0){
+            if(!marked[q1->top().destX][q1->top().destY+1]){
+                Edge up(q1->top().destX,q1->top().destY,q1->top().destX,q1->top().destY+1,max(q1->top().distance,abs(table[q1->top().destX][q1->top().destY]-table[q1->top().destX][q1->top().destY+1])));
+                temp.push_back(up);
+                //cout<<up.distance<<endl;
+            }
+        }
+        if(table[q1->top().destX][q1->top().destY-1]!=0){
+            if(!marked[q1->top().destX][q1->top().destY-1]){
+                Edge down(q1->top().destX,q1->top().destY,q1->top().destX,q1->top().destY-1,max(q1->top().distance,abs(table[q1->top().destX][q1->top().destY]-table[q1->top().destX][q1->top().destY-1])));
+                temp.push_back(down);
+            }
+        }
+        marked[q1->top().destX][q1->top().destY]=true;
+        if(q1->top().destX==dX&&q1->top().destY==dY){
+            return q1->top().distance;
         }
 
-        else if(boxes[to].onStack){
-
-            n.lowLink=min(n.lowLink,boxes[to].index);
-
+        q1->pop();
+        for(itr=temp.begin();itr!=temp.end();++itr){
+            Edge a=*itr;
+            // cout<<a.distance<<endl;
+            q1->push(a);
         }
 
-    }
-
-    if(n.lowLink==n.index) {
-
-        vector<int> a;
-
-        while (boxes[s.top()].data != n.data) {
-
-            boxes[s.top()].onStack = false;
-
-            sameScc[s.top()] = sccCount;
-
-            a.push_back(s.top());
-
-            s.pop();
-
-        }
-
-        boxes[s.top()].onStack = false;
-
-        sameScc[s.top()] = sccCount;
-
-        a.push_back(s.top());
-
-        s.pop();
-
-        members.push_back(a);
-
-        sccCount++;
 
     }
 
 }
-
-int main(int argc,char*argv[]){
-
-    if(argc!=3){
-
-        cout<<"Run the code with the follwing command: ./project3 [input_file] [output_file]"<<endl;
-
+//
+int main(int argc,char*argv[]) {
+    if (argc != 3) {
+        cout << "Run the code with the follwing command: ./project4 [input_file] [output_file]" << endl;
         return 1;
-
     }
-
     ifstream infile(argv[1]);
-
     ofstream outfile;
-
     outfile.open(argv[2]);
-
-    int N;
-
-    infile>>N;
-
-    for(int i=0;i<N;i++){
-
-        int keynum;
-
-        infile>>keynum;
-
-        boxes[i].data=i;
-
-        for(int j=0;j<keynum;j++){
-
-            int num;
-
-            infile>>num;
-
-            num--;
-
-            boxes[i].vector1.push_back(num);
-
+    int n, m;
+    infile >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            int x;
+            infile >> x;
+            table[i][j] = x;
         }
-
     }
+    int q;
+    infile >> q;
+    int sX, sY, dX, dY;
+    infile >> sX >> sY >> dX >> dY;
 
-    for(int i=0;i<N;i++) {
 
-        if (boxes[i].index == -1) {
 
-            strongConnect(boxes[i]);
-
-        }
-
-    }
-
-    bool isChild[sccCount];
-
-    for(int i=0;i<sccCount;i++){
-
-        isChild[i]=false;
-
-    }
-
-    for(int i=0;i<N;i++){
-
-        for(int x:boxes[i].vector1){
-
-            if(sameScc[x]!=sameScc[i]){
-
-                isChild[sameScc[x]]=true;
-
-            }
-
-        }
-
-    }
-
-    vector<int> res;
-
-    int counter=0;
-
-    for(int i=0;i<sccCount;i++){
-
-        if(!isChild[i]){
-
-            counter++;
-
-            res.push_back(members[i].front());
-
-        }
-
-    }
-
-    outfile<<counter<<" ";
-
-    for(int x:res){
-
-        x++;
-
-        outfile<<x<<" ";
-
-    }
-
+    outfile << LeastLeader(sX, sY, dX, dY);
     outfile.close();
 
-    return 0;
 
+    return 0;
 }
